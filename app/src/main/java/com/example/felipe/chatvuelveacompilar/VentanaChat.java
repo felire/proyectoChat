30074a5949b4c236fdf5ad443794e844886d2a77
felire <feliress13@gmail.com>
2017-04-04 01:16:45 -0300

    Subo EditText y button del chat

--- app/src/main/java/com/example/felipe/chatvuelveacompilar/VentanaChat.java
+++ app/src/main/java/com/example/felipe/chatvuelveacompilar/VentanaChat.java
@@ -18,14 +18,14 @@ public class VentanaChat extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         texto = (TextView) findViewById(R.id.nombreCon);
         Contacto contacto = (Contacto) getIntent().getExtras().getSerializable("contacto");
         texto.setText(contacto.getNombre());
